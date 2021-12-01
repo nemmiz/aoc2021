f10f97a32772f2b838943acd5787cbd9805001c3
Emma Norgren <emmanorgren85@gmail.com>
2021-12-01 07:22:57 +0100

    Day 1

--- python/01.py
+++ python/01.py
@@ -1,1 +1,14 @@
-print('Hello')
+with open('../input/01.txt') as stream:
+    d = [int(line) for line in stream.readlines()]
+
+increasing = 0
+for i in range(len(d)):
+    if i > 0 and d[i] > d[i-1]:
+        increasing += 1
+print(increasing)
+
+increasing = 0
+for i in range(len(d)):
+    if i > 3 and (d[i] + d[i-1] + d[i-2]) > (d[i-1] + d[i-2] + d[i-3]):
+        increasing += 1
+print(increasing)
