de428a3a19d504f39586269a0b85949784c855d7
Emma Norgren <emmanorgren85@gmail.com>
2021-12-06 09:28:49 +0100

    Day 6

---
+++ python/06.py
@@ -0,0 +1,17 @@
+def simulate(fish, iterations):
+    fishes = [0] * 9
+    for f in fish:
+        fishes[f] += 1
+    for _ in range(iterations):
+        zeros = fishes[0]
+        for i in range(0, 8):
+            fishes[i] = fishes[i+1]
+        fishes[8] = zeros
+        fishes[6] += zeros
+    print(sum(fishes))
+
+with open('../input/06.txt') as stream:
+    fish = [int(x) for x in stream.read().split(',')]
+
+simulate(fish, 80)
+simulate(fish, 256)
