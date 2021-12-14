450e3f0633c94fbb574144da1b8f218624119290
Emma Norgren <emma.norgren@tieto.com>
2021-12-14 10:13:50 +0100

    Day 14

--- python/14.py
+++ python/14.py
@@ -1,106 +1,37 @@
-def insert(template, rules):
-    result = []
-    for i in range(1, len(template)):
-        pair = template[i-1:i+1]
-        e = rules[pair]
-        if i == 1:
-            result.append(pair[0])
-        result.append(e)
-        result.append(pair[1])
-    return ''.join(result)
-
 def merge(c1, c2):
-    keys = set()
-    keys.update(c1.keys())
-    keys.update(c2.keys())
-    return {k: c1.get(k, 0) + c2.get(k, 0) for k in keys}
+    return {k: c1.get(k, 0) + c2.get(k, 0) for k in set((*c1.keys(), *c2.keys()))}
 
-def counts(pair, rules, rem, cnt, cache):
-    #print(pair, rem)
-    entry = (pair, rem)
+def counts(pair, rules, remaining, cache):
+    entry = (pair, remaining)
     if entry in cache:
         return cache[entry]
 
     e = rules[pair]
-    if rem == 1:
+    if remaining == 1:
         if pair[0] == e:
             return {e: 2}
-        return {
-            pair[0]: 1,
-            e: 1,
-        }
-        #cnt[pair[0]] += 1
-        #cnt[e] += 1
-        #print(pair)
-        #return
+        return {e: 1, pair[0]: 1}
 
-    c1 = counts(f'{pair[0]}{e}', rules, rem-1, cnt, cache)
-    c2 = counts(f'{e}{pair[1]}', rules, rem-1, cnt, cache)
-
-    result = merge(c1, c2)
+    result = merge(
+        counts(f'{pair[0]}{e}', rules, remaining-1, cache),
+        counts(f'{e}{pair[1]}', rules, remaining-1, cache),
+    )
 
     cache[entry] = result
-
     return result
 
-    # keys = set()
-    # keys.update(c1.keys())
-    # keys.update(c2.keys())
-    # return {k: c1.get(k, 0) + c2.get(k, 0) for k in keys}
-
-
-
-def counts_all(template, rules, steps):
-    #letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    #cnt = {c: 0 for c in letters}
+def solve(template, rules, steps):
     cache = {}
-    cnt = {}
     result = {}
     for i in range(1, len(template)):
-        tmp = counts(template[i-1:i+1], rules, steps, cnt, cache)
-        result = merge(tmp, result)
+        result = merge(result, counts(template[i-1:i+1], rules, steps, cache))
     result[template[-1]] += 1
-    #print(result)
-    #print('Cache size:', len(cache))
-
-    vals = sorted(result.values())
-    print(vals[-1] - vals[0])
-
-    # cnt[template[-1]] += 1
-    # for c in letters:
-    #     n = cnt.get(c, 0)
-    #     if n > 0:
-    #         print(c, n)
-
-def countchars(template):
-    uniq = set(template)
-    counts = [(template.count(c), c) for c in uniq]
-    counts.sort()
-    print(counts)
+    print(max(result.values()) - min(result.values()))
 
 with open('../input/14.txt') as stream:
     lines = [line.strip() for line in stream.readlines()]
     template = lines[0]
-    rules = {}
-    for line in lines[2:]:
-        a, b = line.split(' -> ')
-        rules[a] = b
+    rules = dict([line.split(' -> ') for line in lines[2:]])
 
-#print(template)
-#print(rules)
-
-counts_all(template, rules, 10)
-counts_all(template, rules, 40)
-
-# for i in range(15):
-#     print(i, template[:4])
-#     #print(i, len(template))
-#     template = insert(template, rules)
-    
-# #print(template)
-
-# uniq = set(template)
-# counts = [(template.count(c), c) for c in uniq]
-# counts.sort()
-
-# print(counts[-1][0] - counts[0][0])
+solve(template, rules, 10)
+solve(template, rules, 40)
